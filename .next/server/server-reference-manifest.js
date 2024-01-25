c20f9a25067ce9512c3a682e448db10d1029abad
Leah McBeth <leahloughran@gmail.com>
2024-01-25 20:05:31 +0000

    initial commit

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"iTePmSvM1zbc3oFTLFqjGCrhyQEKaU0rodyYNcgzwts=\"\n}"
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Page() {
+  return <div>
+    <h1>Backed by STX</h1>
+
+  </div>
+}
